3a18bbe189010b38654451a223a804336e552e12
TutTrue <hamdy.xd55@gmail.com>
2023-06-10 04:20:28 -0700

    13-is_palindrome another way

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,68 +1,46 @@
 #include "lists.h"
 /**
+ * listint_len - length of linked list
+ * @h: head of the list
+ * Return: length of a list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *p;
+	int counter = 0;
+
+	p = h;
+	while (p)
+	{
+		counter++;
+		p = p->next;
+	}
+	return (counter);
+}
+/**
  * reverse_listint - reverse a list
  * @head: head of the list
  * Return: pointer to the first node
  */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *cur, *prev;
-
-	if (!*head)
-		return (NULL);
-	cur  = (*head)->next;
-	prev = NULL;
-	while (cur)
-	{
-		(*head)->next = prev;
-		prev = *head;
-		*head = cur;
-		cur = cur->next;
-	}
-	(*head)->next = prev;
-	return (*head);
-}
-/**
- * is_palindrome -  checks if a singly linked list is a palindrome
- * @head: head of the list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *forw = *head, *backw = *head, *temp;
-	int i, counter = 0, flag = 1;
+	int i, j, len, flag = 1;
+	listint_t *temp = *head;
 
-	if (!head)
+	if (!*head)
+		return (flag);
+	len = listint_len(*head);
+	int *arr = malloc(sizeof(int) * len);
+	for (i = 0; i < len; i++)
 	{
-		flag = 0;
-		goto here;
+		arr[i] = temp->n;
+		temp = temp->next;
 	}
-	while (!backw)
+	for (i = 0, j = len - 1; i < len; i++)
 	{
-		counter++;
-		backw = backw->next;
-	}
-	backw = forw;
-	for (i = 1; i < (counter / 2); i++)
-		backw = backw->next;
-	temp = backw->next;
-	backw->next = NULL;
-	backw = temp;
-	reverse_listint(&backw);
-	while (backw && forw)
-	{
-		if (backw->n != forw->n)
-		{
+		if (arr[i] != arr[j])
 			flag = 0;
-			break;
-		}
-		backw = backw->next;
-		forw = forw->next;
 	}
-	reverse_listint(&backw);
-	while (forw->next != NULL)
-		forw = forw->next;
-	forw->next = backw;
-here:
+	free(arr);
 	return (flag);
 }
