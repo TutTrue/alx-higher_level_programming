7f3738c770c757eb7f40b7ae56c8bfa982822071
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-05 09:51:12 +0300

    2-matrix_divided.py pycodestyle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,7 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list)
             or not all(isinstance(row, list) for row in matrix)
             or not all((isinstance(num, int) or isinstance(num, float))
-                for row in matrix for num in row)):
+                       for row in matrix for num in row)):
         message = "matrix must be a matrix (list of lists) of integers/floats"
         raise TypeError(message)
     if not all(len(matrix[0]) == len(li) for li in matrix):
