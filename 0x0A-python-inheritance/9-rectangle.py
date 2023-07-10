6ca0cf6ad3eab755d3c215fe90c5b70dd996be13
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-10 17:05:23 +0300

    9-rectangle.py super() not self

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,9 +7,9 @@ class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
         """init the height and width"""
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
         def area(self):
@@ -18,4 +18,4 @@ class Rectangle(BaseGeometry):
 
         def __str__(self):
             """print format of the rectangle"""
-            return f"[Rectangle] {self.__width:s}/{self.__height:s}"
+            return f"[Rectangle] {self.__width}/{self.__height}"
