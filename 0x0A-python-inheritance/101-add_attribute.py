274633e773e54300de97358345360e1ab333fb4f
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-07-10 15:39:09 +0300

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""add a new attri if valid"""
+
+
+def add_attribute(obj, att, value):
+    """add a new attribute and a value if valid"""
+    if "__dic___" in dir(obj):
+        setattr(obj, att, value)
+    else:
+        raise TypeError("can't add new attribute")
