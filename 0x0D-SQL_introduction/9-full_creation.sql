af707ed96552dbc4708cf09279e2a736daaf31a0
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-15 19:48:32 +0300

    fix 9 and 16

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,5 @@
 -- creates a table second_table and add some rows
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES (1, 'Jhon', 10);
 INSERT INTO second_table VALUES (2, 'Alex', 3);
 INSERT INTO second_table VALUES (3, 'Bob', 14);
