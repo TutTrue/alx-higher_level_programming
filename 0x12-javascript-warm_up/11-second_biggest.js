3abeb1435d5858f9c7c6a03206333f2a20776765
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-09-11 22:20:39 +0300

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 if (process.argv.length === 2 || process.argv.length === 3) { console.log(0); } else {
-  let max = process.argv[2];
-  let second = process.argv[3];
-  process.argv.forEach(el => {
-    if (el > max) { max = el; }
-    if (el > second && el !== max) { second = el; }
-  });
-  console.log(second);
+  process.argv.sort((a, b) => b - a);
+  console.log(process.argv[3]);
 }
