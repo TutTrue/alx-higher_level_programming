52bb147e5899ec7b4c37b1f41105cf81f2fdf6e5
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-09-28 23:54:49 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # takes a url sends a request and display the content-length
-curl -i "$1" | grep content-length | grep -o '[0-9]\+'
+curl -sI "$1" | grep content-length | grep -o '[0-9]\+'
