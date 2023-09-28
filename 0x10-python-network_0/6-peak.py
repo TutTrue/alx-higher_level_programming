68f289b67bddb2837f95b7ec7a6a3cf63b22e2c0
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-09-29 00:54:25 +0300

    pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
+"""function that finds a peak of the array"""
+
+
 def find_peak(list_of_integers):
     """find a peak"""
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
     for i in range(len(list_of_integers)):
         try:
-            if list_of_integers[i - 1] <= list_of_integers[i] and list_of_integers[i] >= list_of_integers[i+1]:
+            if list_of_integers[i - 1] <= list_of_integers[i] and
+                    list_of_integers[i] >= list_of_integers[i+1]:
                 return list_of_integers[i]
-        except:
+        except IndexError:
             try:
                 if list_of_integers[i] >= list_of_integers[i+1]:
                     return list_of_integers[i]
-            except:
+            except IndexError:
                 if list_of_integers[i] > list_of_integers[i - 1]:
                     return list_of_integers[i]
     return None
