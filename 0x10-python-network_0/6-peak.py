528902b2dfed742239c962363cc3b154adcb77ba
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-09-29 00:57:19 +0300

    fix pycodestyle of peak function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,9 +8,9 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     for i in range(len(list_of_integers)):
         try:
-            if list_of_integers[i - 1] <= list_of_integers[i] and
-                    list_of_integers[i] >= list_of_integers[i+1]:
-                return list_of_integers[i]
+            if list_of_integers[i - 1] <= list_of_integers[i]:
+                if list_of_integers[i + 1] <= list_of_integers[i]:
+                    return list_of_integers[i]
         except IndexError:
             try:
                 if list_of_integers[i] >= list_of_integers[i+1]:
