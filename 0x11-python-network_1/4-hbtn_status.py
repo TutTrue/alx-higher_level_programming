8f26abad85d849ae8a04006e6c00cab5fe18e195
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-09-29 18:45:50 +0300

    get request with requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetch an url using requests package"""
+
+
+if __name__ == '__main__':
+    import requests
+
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print(f'\t- type: {type(res.text)}')
+    print(f'\t- content: {res.text}')
