c75cd36f95a678630788e6a1c5d22bb70ea851a6
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-09-30 02:53:40 +0300

    get last 10 commits sha and author name using github api

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    url = f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits'
+    url = f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits'
     req = requests.get(url)
     res = req.json()
     for commit in res[:10]:
-        print(f'{commit.get("sha")}: {commit.get("commit").get("author").get("name")}')
+        print(f'{commit.get("sha")}: '
+              f'{commit.get("commit").get("author").get("name")}')
