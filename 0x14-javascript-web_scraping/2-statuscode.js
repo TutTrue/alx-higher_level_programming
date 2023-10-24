62f2c80358f426b4bc80f60b32e122805240b859
TutTrue <hamdy.xd55@gmail.com>
2023-10-24 14:41:40 -0700

    fix format

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request.get(process.argv[2], (error, response, body) => {
-	console.log("code " + response.statusCode)
+	console.log("code: " + response.statusCode)
 })
