50ad676adc59e564b03c7cf3ebd335f2b68c33a1
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-24 19:51:20 +0300

    semistandard check

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,9 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
     console.err(err);
   } else {
